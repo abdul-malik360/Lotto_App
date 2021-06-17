5361cb5c82f6669cba754418b3390de9df9aae82
abdul-malik360 <82204978+abdul-malik360@users.noreply.github.com>
2021-06-17 03:12:11 +0200

    pictures are buttons functions

---
+++ prac2.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+
+root = Tk()
+root.geometry("600x600")
+root.title("LOTTO GAME")
+root.config(bg="#FFC107")
+
+
+def choose_number():
+    my_label.config(text="1")
+
+number_pic = PhotoImage(file="1.PNG")
+numb_label = Label(image=number_pic)
+
+btn = Button(root, image=number_pic, command=choose_number, borderwidth=0)
+btn.place(x=50, y=150)
+
+my_label = Label(root,text="")
+my_label.place(x=50, y=250)
+
+
+
+
+root.mainloop()
